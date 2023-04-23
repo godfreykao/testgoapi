99ce8121b77d5ff32fd63bdd18c46e67a7622fdd
godfrey <>
2023-04-23 22:05:33 +0800

    Add package for debugging

--- controller/wallet/controller.go
+++ controller/wallet/controller.go
@@ -4,15 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"testgoapi/util"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/lgbya/go-dump"
 )
 
 func UpdateBalance(c *fiber.Ctx) error {
 	params := struct {
 		BrandId       uint              `json:"brandid" validate:"required"`
-		PlayerId      uint              `json:"playerid"`
-		SessionId     string            `json:"sessionid" validate:"required,alphanum,max:10"`
+		PlayerId      uint              `json:"playerid" validate:"required"`
+		SessionId     string            `json:"sessionid" validate:"required,alphanum,max=10"`
 		Amount        float64           `json:"amount" validate:"gte=0,lte=10000000000"`
 		Currency      string            `json:"currency"`
 		TransactionId string            `json:"transactionid"`
@@ -25,8 +28,26 @@ func UpdateBalance(c *fiber.Ctx) error {
 		return err
 	}
 
+	err := validator.New().Struct(&params)
+
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		if len(validationErrors) > 0 {
+			dump.Printf(validationErrors[0])
+		}
+	}
+
+	if true {
+		panic("dopanic")
+	}
+
+	util.VarDump(params)
+	arr := map[string]int{"foo": 1, "name": 2}
+	util.VarDump(arr)
+
 	if err := validator.New().Struct(&params); err != nil {
 		//return c.JSON(err.Error())
+		fmt.Printf("%T", err.Error())
+		//fmt.Println(err.Error())
 		return c.SendString(err.Error())
 	}
 
